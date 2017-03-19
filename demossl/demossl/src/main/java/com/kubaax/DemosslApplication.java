8d73cad9b78686a5831b4d2c576364808fa16ce5
serigne dieng <kayeldieng@gmail.com>
2017-03-19 12:18:20 +0100

    firt commit

---
+++ demossl/demossl/src/main/java/com/kubaax/DemosslApplication.java
@@ -0,0 +1,12 @@
+package com.kubaax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosslApplication.class, args);
+	}
+}
